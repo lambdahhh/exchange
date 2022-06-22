6b2f55af668e462469c1d9454f3ee8ae2a505b45
pvolkov <p.volkov@ndv.ru>
2022-06-22 17:23:54 +0300

    WIP

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,16 +3,19 @@ import Header from "../header/header";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Rates from "../rates/rates";
 import Converter from "../converter/converter";
+import './app.css';
 
 const App = () => {
     return (
             <BrowserRouter>
                 <Header/>
-                <Routes>
-                    <Route path="/" element={<Rates/>}/>
-                    <Route path="/rates" element={<Rates/>}/>
-                    <Route path="/converter" element={<Converter/>}/>
-                </Routes>
+                <div className="content">
+                    <Routes>
+                        <Route path="/" element={<Rates/>}/>
+                        <Route path="/rates" element={<Rates/>}/>
+                        <Route path="/converter" element={<Converter/>}/>
+                    </Routes>
+                </div>
             </BrowserRouter>
     )
 }
